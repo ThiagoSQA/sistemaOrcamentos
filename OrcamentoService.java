bbb2d2604cecd3ecb860ba682f941c6283a12b13
Fernanda Pita <163304536+tsukitta@users.noreply.github.com>
2025-06-27 14:38:06 -0300

    Update OrcamentoService.java

--- OrcamentoService.java
+++ OrcamentoService.java
@@ -2,14 +2,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OrcamentoService {
+    // lista que armazena os orcamentos temporariamente
     private List<Orcamento> orcamentos = new ArrayList<>();
 
+    // salva um orcamento, exporta para arquivo e limpa a lista
     public void salvarOrcamento(Orcamento o) {
         orcamentos.add(o);
         FileExporter.exportarParaArquivo(orcamentos);
         orcamentos.clear();
     }
-    public void excluirorcamento(Orcamento a){
+
+    // remove um orcamento da lista
+    public void excluirorcamento(Orcamento a) {
         orcamentos.remove(a);
     }
 }
