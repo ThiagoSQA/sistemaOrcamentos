c451494564e30168cc6dd2037452777450573eb6
Fernanda Pita <163304536+tsukitta@users.noreply.github.com>
2025-06-27 14:34:07 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+        //cria uma instancia da interface grafica
         SimpleGUI gui = new SimpleGUI();
+        
+        //exibe a interface grafica na tela
         gui.exibir();
-
     }
 }
