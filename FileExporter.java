fe681f3f3f408ec382e5aa592bce431ec8b8cce2
Fernanda Pita <163304536+tsukitta@users.noreply.github.com>
2025-06-27 14:35:25 -0300

    Update FileExporter.java

--- FileExporter.java
+++ FileExporter.java
@@ -3,12 +3,16 @@ import java.io.IOException;
 import java.util.List;
 
 public class FileExporter {
+    // metodo que exporta uma lista de orcamentos para um arquivo
     public static void exportarParaArquivo(List<Orcamento> orcamentos) {
+        // tenta abrir o arquivo em modo de acrescentar conteudo
         try (FileWriter writer = new FileWriter("orcamentos.txt", true)) {
+            // escreve cada orcamento no arquivo
             for (Orcamento o : orcamentos) {
                 writer.write(o.toString() + "\n");
             }
         } catch (IOException e) {
+            // imprime erro caso ocorra problema ao escrever no arquivo
             e.printStackTrace();
         }
     }
