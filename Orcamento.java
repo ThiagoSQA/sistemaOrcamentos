fb43aa1ba35b42b85e5518549f7039edb64ebde4
Fernanda Pita <163304536+tsukitta@users.noreply.github.com>
2025-06-27 14:36:49 -0300

    Update Orcamento.java

--- Orcamento.java
+++ Orcamento.java
@@ -1,20 +1,25 @@
 public class Orcamento {
+    // atributos do orcamento: nome do cliente e valor
     private String cliente;
     private double valor;
 
+    // construtor que recebe cliente e valor
     public Orcamento(String cliente, double valor) {
         this.cliente = cliente;
         this.valor = valor;
     }
 
+    // retorna o nome do cliente
     public String getCliente() {
         return cliente;
     }
 
+    // retorna o valor do orcamento
     public double getValor() {
         return valor;
     }
 
+    // representa o orcamento como texto
     @Override
     public String toString() {
         return cliente + ": R$ " + valor;
